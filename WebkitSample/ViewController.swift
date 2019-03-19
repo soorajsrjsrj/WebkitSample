7414e5b78b1b8919c25dbae467c1dfac7c325c51
sooraj <soorajsuneesh@gmail.com>
2019-03-18 23:33:19 -0400

    swift project for webkit

--- WebkitSample/ViewController.swift
+++ WebkitSample/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var myWeb: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //myWeb.navigationDelegate = self as! WKNavigationDelegate
+       // myWeb.allowsBackForwardNavigationGestures = true
+        loadFromString()
+        
     }
-
-
+    
+    func loadFromString()
+    {
+        let htmlStr = "<h1>Hello World</h1>"
+        myWeb.loadHTMLString(htmlStr, baseURL: nil)
+    }
+    
+    func loadFromFile()
+    {
+        let localfilePath = Bundle.main.url(forResource: "home", withExtension: "html")
+        let myRequest = URLRequest(url: localfilePath!)
+        myWeb.load(myRequest)
+        
+    }
+    
+    func loadFromUrl()
+    {
+        //let url = URL(string: "https://www.youtube.com/watch?v=xQmZSKxOYvs")
+        let url = URL(string: "https://www.google.com")
+        let urlReq = URLRequest(url: url!)
+        myWeb.load(urlReq)
+    }
+    
+    
+    /*
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
-
